cda3fbd21049c264a7af184964853db1e0ff0d23
OmerDiaaeldin <omardiaaeldin2592001@gmail.com>
2024-12-09 17:36:28 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int INF = 4000;
+
+int n;
+vector<vector<int>> affinity;
+vector<vector<int>> adj;
+
+int bfs(int s, int t, vector<int>& parent) {
+    fill(parent.begin(), parent.end(), -1);
+    parent[s] = -2;
+    queue<pair<int, int>> q;
+    q.push({s, INF});
+
+    while (!q.empty()) {
+        int cur = q.front().first;
+        int flow = q.front().second;
+        q.pop();
+
+        for (int next : adj[cur]) {
+            if (parent[next] == -1 && affinity[cur][next]) {
+                parent[next] = cur;
+                int new_flow = min(flow, affinity[cur][next]);
+                if (next == t)
+                    return new_flow;
+                q.push({next, new_flow});
+            }
+        }
+    }
+
+    return 0;
+}
+
+int maxflow(int s, int t) {
+    int flow = 0;
+    vector<int> parent(n);
+    int new_flow;
+
+    while (new_flow = bfs(s, t, parent)) {
+        flow += new_flow;
+        int cur = t;
+        while (cur != s) {
+            int prev = parent[cur];
+            affinity[prev][cur] -= new_flow;
+            affinity[cur][prev] += new_flow;
+            cur = prev;
+        }
+    }
+
+    return flow;
+}
